83f080ccd67dbbeac8f0c655d815917f458469f0
shankar7042 <46479982+shankar7042@users.noreply.github.com>
2025-08-31 13:25:46 +0530

    setting up the express server and api gateway with proxy

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,18 +1,41 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
-import * as path from "path";
+import proxy from "express-http-proxy";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan";
 
 const app = express();
 
-app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
 
-app.get("/api", (req, res) => {
+app.use(morgan("dev"));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cookieParser());
+app.set("trust proxy", 1);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: (req: any) => (req.user ? 1000 : 100),
+  message: { error: "Too many requests, please try again later!" },
+  standardHeaders: true,
+  legacyHeaders: true,
+});
+
+app.use(limiter);
+
+app.get("/gateway-health", (req, res) => {
   res.send({ message: "Welcome to api-gateway!" });
 });
+
+app.use("/", proxy("http://localhost:6001"));
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,24 @@
+import cors from "cors";
 import express from "express";
 
-const host = process.env.HOST ?? "localhost";
-const port = process.env.PORT ? Number(process.env.PORT) : 6001;
-
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send({ message: "Hello API" });
 });
 
-app.listen(port, host, () => {
+const host = process.env.HOST ?? "localhost";
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+
+const server = app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
+server.on("error", (err) => console.log("Server Error: ", err));
